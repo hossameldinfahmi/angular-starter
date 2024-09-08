4a8b7167e6806b3250e92bacabc53a34b5c94ade
hossameldinfahmi <hossameldinfahmi@gmail.com>
2024-09-08 19:05:54 +0300

    first commit

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from './components/button/button.component';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    ButtonComponent
+  ],
+  exports: [
+    ButtonComponent
+  ]
+})
+export class SharedModule { }
